34391a2b5b8926f1558bd6770cc12002cb81852d
Kalpesh Khairnar <71657135+khairnarkalpesh@users.noreply.github.com>
2022-09-25 23:35:39 +0530

    price and category filter added

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -10,13 +10,17 @@ import {
 } from "../constants/productConstants";
 
 export const getProducts =
-  (keyword = "") =>
+  (keyword = "", currentPage = 1, price = [0, 25000], category) =>
   async (dispatch) => {
     try {
       console.log(keyword[0]);
       dispatch({ type: ALL_PRODUCT_REQUEST });
 
-      let url = `/api/v1/products?keyword=${keyword}`;
+      let url = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}`;
+
+      if(category){
+      url = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}`;
+      }
 
       const { data } = await axios.get(url);
 
--- src/component/Product/Products.js
+++ src/component/Product/Products.js
@@ -6,23 +6,49 @@ import Loader from "../layout/Loader/Loader";
 import ProductCard from "../Home/ProductCard";
 import { useParams } from "react-router-dom";
 import Pagination from "react-js-pagination";
+import Slider from "@mui/material/Slider";
+
+const categories = [
+  "laptop",
+  "footwear",
+  "bottom",
+  "tops",
+  "attire",
+  "camera",
+  "smarthphones",
+  "mobile",
+];
 
 const Products = () => {
   const dispatch = useDispatch();
   const { keyword } = useParams();
   const [currentPage, setCurrentPage] = useState(1);
 
-  const { products, loading, error, productsCount, resultPerPage } =
-    useSelector((state) => state.products);
+  const [price, setPrice] = React.useState([0, 2500]);
+  const [category, setCategory] = useState("");
+
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
+
+  const {
+    products,
+    loading,
+    error,
+    productsCount,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
 
   const setCurrentPageNo = (e) => {
-    console.log(e)
     setCurrentPage(e);
   };
 
   useEffect(() => {
-    dispatch(getProducts(keyword));
-  }, [dispatch, keyword]);
+    dispatch(getProducts(keyword, currentPage, price, category));
+  }, [dispatch, keyword, currentPage, price, category]);
+
+  let count = filteredProductsCount;
 
   return (
     <Fragment>
@@ -33,28 +59,57 @@ const Products = () => {
           <h2 className="productsHeading">Products</h2>
           <div className="products">
             {products ? (
-              products.map((product) => <ProductCard product={product} />)
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))
             ) : (
               <p>Product Not Found!</p>
             )}
           </div>
 
-          <div className="paginationBox">
-            <Pagination
-              activePage={currentPage}
-              itemsCountPerPage={resultPerPage}
-              totalItemsCount={productsCount}
-              onChange={setCurrentPageNo}
-              nextPageText="Next"
-              prevPageText="Prev"
-              firstPageText="1st"
-              lastPageText="Last"
-              itemClass="page-item"
-              linkClass="page-link"
-              activeClass="pageItemActive"
-              activeLinkClass="pageLinkActive"
+          <div className="filterBox">
+            <p>Price</p>
+            <Slider
+              getAriaLabel={() => "Temperature range"}
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              min={0}
+              max={25000}
             />
+
+            <p>Category</p>
+            <div className="categoryBox">
+              {categories.map((category) => (
+                <li
+                  className="category-link"
+                  key={category}
+                  onClick={() => setCategory(category)}
+                >
+                  {category}
+                </li>
+              ))}
+            </div>
           </div>
+
+          {resultPerPage < productsCount && (
+            <div className="paginationBox">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resultPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText="Next"
+                prevPageText="Prev"
+                firstPageText="1st"
+                lastPageText="Last"
+                itemClass="page-item"
+                linkClass="page-link"
+                activeClass="pageItemActive"
+                activeLinkClass="pageLinkActive"
+              />
+            </div>
+          )}
         </Fragment>
       )}
     </Fragment>
